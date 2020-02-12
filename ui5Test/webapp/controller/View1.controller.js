07e538a8580b4a4d3da7108567bb68ade0e849bd
gustavosimil <gustavo.simil@vaees.com>
2020-02-12 02:13:47 +0000

    First project commit

---
+++ ui5Test/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("com.vaees.ui5Test.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ ui5Test/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/vaees/ui5Test/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
